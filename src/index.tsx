34864df9f4f8912ae914bf6dadbf6be361267a7c
Eunsang <nicolaus1207@gmail.com>
2020-11-18 19:52:09 +0900

    boilerplate

--- src/index.tsx
+++ src/index.tsx
@@ -1,45 +1,19 @@
+// This file is not for actual exercise
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
-interface AppProps {
-  color?: string;
-}
+import { App } from './components/App';
+import { reducers } from './reducers';
 
-// interface AppState {
-//   counter: number;
-// }
+const store = createStore(reducers, applyMiddleware(thunk));
 
-class App extends React.Component<AppProps> {
-  state = { counter: 0 };
-  // above line redefines(overrides) the S for Component
-  // if you want to use constructor then u provide generic for state
-
-  // constructor(props: AppProps) {
-  //   super(props);
-  //   this.state = { counter: 0 };
-  // }
-
-  onIncrement = (): void => {
-    this.setState({ counter: this.state.counter + 1 });
-  };
-
-  onDecrement = (): void => {
-    this.setState({ counter: this.state.counter - 1 });
-  };
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.onIncrement}>Increment</button>
-        <button onClick={this.onDecrement}>Decrement</button>
-        {this.state.counter}
-      </div>
-    );
-  }
-}
-
-// const App = (props: AppProps): JSX.Element => {
-//   return <div>{props.color}</div>;
-// };
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
