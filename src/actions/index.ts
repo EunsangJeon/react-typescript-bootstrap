4055502387153095d8ec3ae4288efb3b81a80c20
Eunsang <nicolaus1207@gmail.com>
2020-11-18 20:55:34 +0900

    fetch todos done

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -16,7 +16,7 @@ export interface FetchTodosAction {
 
 const url = 'https://jsonplaceholder.typicode.com/todos';
 
-export const fecthTodos = () => {
+export const fetchTodos = () => {
   return async (dispatch: Dispatch) => {
     const response = await axios.get<Todo[]>(url);
 
