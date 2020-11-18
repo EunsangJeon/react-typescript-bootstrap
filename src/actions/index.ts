a5a05bd9b1f96f662192f19309ca34fb5b8637f5
Eunsang <nicolaus1207@gmail.com>
2020-11-18 20:14:34 +0900

    fetchTodos action done

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -8,13 +8,18 @@ interface Todo {
   completed: boolean;
 }
 
+interface FetchTodosAction {
+  type: ActionTypes.fetchTodos;
+  payload: Todo[];
+}
+
 const url = 'https://jsonplaceholder.typicode.com/todos';
 
 export const fecthTodos = () => {
   return async (dispatch: Dispatch) => {
     const response = await axios.get<Todo[]>(url);
 
-    dispatch({
+    dispatch<FetchTodosAction>({
       type: ActionTypes.fetchTodos,
       payload: response.data,
     });
